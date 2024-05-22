44588334edf549a13e45fc10c46e1fbfce2fc744
junealexis13 <junealexis.santos13@gmail.com>
2024-05-22 16:17:01 +0800

    creation of inventory management webapp for my enjel

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def landing_page(request):
+    return render(request, "home/home.html")
+
+def login_page(request):
+    return render(request, "home/login.html")
+
+def dashboard(request):
+    return render(request, "home/dashboard.html")
