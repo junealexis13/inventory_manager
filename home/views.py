c311d6085671a11bc4d091beb2ede84387e08340
junealexis13 <junealexis.santos13@gmail.com>
2024-05-25 11:40:30 +0800

    minor design changes

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def landing_page(request):
-    return render(request, "home/homepage.html", {})
+    return render(request, "blocks/homepage_blocks.html", {})
 
 def login_page(request):
     return render(request, "home/login.html", {})
