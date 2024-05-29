3926a5aae21e784d3bc5942a48e50e33169c514c
junealexis13 <junealexis.santos13@gmail.com>
2024-05-29 11:12:40 +0800

    included the app gallery menu

---
+++ home/static/js/navbar.js
@@ -0,0 +1,8 @@
+function toggleDiv() {
+    var div = document.getElementById("extend");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
