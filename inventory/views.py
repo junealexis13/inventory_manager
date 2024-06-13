9d0cee74d4938a02faa61cd42b642f2d7edb8199
junealexis13 <junealexis.santos13@gmail.com>
2024-06-13 15:47:36 +0800

    changes in inventory view

--- inventory/views.py
+++ inventory/views.py
@@ -4,6 +4,6 @@ from home.models import Stock, Category, Supplier, Product
 # Create your views here.
 def inventory_dashboard(request):
     var = {}
-
-
+    var['inventory'] = Stock.objects.all()
+    var['inventory_count'] = Stock.objects.count()
     return render(request, 'blocks/dashboard_components.html',var)  
