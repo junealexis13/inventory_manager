26e11b1a1cf6800531cef0afcf493740f0ed0751
junealexis13 <junealexis.santos13@gmail.com>
2024-09-01 22:08:40 +0800

    remove vercel footprints

--- inventory_manager/settings.py
+++ inventory_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-q(*i#5fvs%!99)=az!a#d2oj=dgyayg)@dgln8e+cjq%@+(o(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
