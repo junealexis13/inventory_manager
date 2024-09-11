1d766d52ca287b88528478de8e91df34c503347d
junealexis13 <junealexis.santos13@gmail.com>
2024-09-11 17:54:55 +0800

    major ui change in financial information

--- transactions/views.py
+++ transactions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.dispatch import receiver
 from django.contrib import messages
 from django.db.models.signals import post_save
@@ -9,10 +10,11 @@ from django.views.decorators.http import require_POST
 from home.models import Stock
 from transactions.models import SellItem
 
+
 from plotly.offline import plot
 import plotly.express as px
 import pandas as pd 
-
+from datetime import datetime, timedelta
 
 @require_POST
 def get_total_price(request):
@@ -26,24 +28,93 @@ def update_stock_is_sold(sender, instance, **kwargs):
     instance.stock_items.update(is_sold=True)
     instance.stock_items.update(status='not_available')
 
+def modify_update_layout(fig, color='RebeccaPurple'):
+    fig.update_layout(
+        title_font=dict(size=24, family='Arial', color='black'),
+        xaxis_title="",
+        yaxis_title="Revenue",
+        xaxis=dict(tickangle=-90),
+        font=dict(family="Courier New, monospace", size=10, color=color)
+    )
+
+    fig.update_layout(
+        height=400
+    )
+
+    fig.update_traces(marker_color=color)
+
+
 def transactions_dashboard(request):
     total_price = 0
     transactions = SellItem.objects.all()
     tx_count = transactions.count()
 
+    now = timezone.now()
+    year_now = datetime.now().year
+    day_ago = now - timedelta(days=1)
+
     if tx_count != 0:
-        sellitem_data = [
+        day_revenue = [
+                    {
+                        'price': x.total_price,
+                        'date_transaction': x.date_transaction
+                    } for x in transactions if day_ago <= x.date_transaction <= now
+                ]
+
+
+        all_revenue = [
             {
                 'price': x.total_price,
                 'date_transaction': x.date_transaction
             } for x in transactions
         ]
 
-        df = pd.DataFrame(sellitem_data)
-        fig = px.bar(df, x='date_transaction', y='price')
-        sold_data = plot(fig, output_type="div")
+        quarter_revenue = [
+            {
+                'price': sum([float(x.total_price) for x in transactions 
+                            if x.date_transaction >= timezone.make_aware(datetime(year_now, 1, 1)) 
+                            and x.date_transaction <= timezone.make_aware(datetime(year_now, 3, 31))]),
+                'quarter': 'First Quarter'
+            },
+            {
+                'price': sum([float(x.total_price) for x in transactions 
+                            if x.date_transaction >= timezone.make_aware(datetime(year_now, 4, 1)) 
+                            and x.date_transaction <= timezone.make_aware(datetime(year_now, 6, 30))]),
+                'quarter': 'Second Quarter'
+            },
+            {
+                'price': sum([float(x.total_price) for x in transactions 
+                            if x.date_transaction >= timezone.make_aware(datetime(year_now, 7, 1)) 
+                            and x.date_transaction <= timezone.make_aware(datetime(year_now, 9, 30))]),
+                'quarter': 'Third Quarter'
+            },
+            {
+                'price': sum([float(x.total_price) for x in transactions 
+                            if x.date_transaction >= timezone.make_aware(datetime(year_now, 10, 1)) 
+                            and x.date_transaction <= timezone.make_aware(datetime(year_now, 12, 31))]),
+                'quarter': 'Fourth Quarter'
+            }
+        ]
+
+
+        df1 = pd.DataFrame(all_revenue)
+        df2 = pd.DataFrame(day_revenue)
+        df3 = pd.DataFrame(quarter_revenue)
+        fig1 = px.bar(df1, x='date_transaction', y='price',title='All-Time Revenue')
+        modify_update_layout(fig1, color='navy')
+        fig2 = px.bar(df2, x='date_transaction', y='price',title='Daily Revenue')
+        modify_update_layout(fig2, color='teal')
+        fig3 = px.bar(df3, x='quarter', y='price',title=f'Quarterly Revenue: {str(year_now)} ')
+        modify_update_layout(fig3, color='maroon')
+        sold_data_all = plot(fig1, output_type="div")
+        sold_data_daily = plot(fig2, output_type="div")
+        sold_data_quarterly = plot(fig3, output_type="div")
+
+        print(quarter_revenue)
+
+
     else:
-        sold_data = None
+        sold_data_all, sold_data_daily,sold_data_quarterly= None
 
     if request.method == 'POST':
         form = SellItemForm(request.POST)
@@ -56,4 +127,7 @@ def transactions_dashboard(request):
 
     return render(request, 'blocks/transaction_components.html',
         {'form': form, 'total_price': total_price, 'transactions': transactions, 
-        'transaction_count': tx_count, "plotly_sold_data": sold_data})
+        'transaction_count': tx_count, 
+        "plotly_sold_all": sold_data_all,
+        "plotly_sold_daily": sold_data_daily,
+        "plotly_sold_quarterly": sold_data_quarterly})
