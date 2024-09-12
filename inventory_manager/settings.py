76fa200e23fbf7e799d17e0dffe783714343d847
junealexis13 <junealexis.santos13@gmail.com>
2024-09-12 10:17:21 +0800

    minor changes for vercel deployment

--- inventory_manager/settings.py
+++ inventory_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-q(*i#5fvs%!99)=az!a#d2oj=dgyayg)@dgln8e+cjq%@+(o(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['junealexis13.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['junealexis13.pythonanywhere.com','.vercel.app','127.0.0.1']
 
 
 # Application definition
